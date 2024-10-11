c42fe2b2b4e4d9141f25b96c69d8d7b9f1f14b2d
Kenneth Lareau <relayer@lindenlab.com>
2024-10-10 17:39:49 -0700

    Completed chapter 7

---
+++ chapter_7/restaurant/done/lib_2.rs
@@ -0,0 +1,39 @@
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
+}
+
+pub use crate::front_of_house::hosting;
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+
+mod customer {
+    pub fn eat_at_restaurant() {
+        super::hosting::add_to_waitlist();
+    }
+}
